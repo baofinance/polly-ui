d16c76a3b28ac891e427e8b34a066c1bf968a1d1
RB <75801879+rocky-baoboa@users.noreply.github.com>
2021-03-12 11:40:03 -0500

    add href property for styled link (#17)

--- src/components/Footer/components/Nav.tsx
+++ src/components/Footer/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav: React.FC = () => {
 		<StyledNav>
 			<StyledLink
 				target="_blank"
-				https://blockscout.com/poa/xdai/address/0xf712a82DD8e2Ac923299193e9d6dAEda2d5a32fd/"
+				href="https://blockscout.com/poa/xdai/address/0xf712a82DD8e2Ac923299193e9d6dAEda2d5a32fd/"
 			>
 				BaoChef Contract
 			</StyledLink>
