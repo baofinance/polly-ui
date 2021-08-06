960735f3bd30d1a3205291e1e4cbe5d0687a0b2e
Vital <34991917+Vitalsine85@users.noreply.github.com>
2021-08-05 22:01:16 -0400

    lottie installed

--- src/views/Home/components/FullPage.js
+++ src/views/Home/components/FullPage.js
@@ -5,6 +5,9 @@ import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverfl
 import ReactFullpage from '@fullpage/react-fullpage'
 import lines from '../../../assets/img/bg-lines.png'
 import SkyHero from './SkyHero'
+import Lottie from "react-lottie"
+import '@lottiefiles/lottie-player';
+import { create } from '@lottiefiles/lottie-interactivity';
 
 import './fullpage.style.css'
 
@@ -14,7 +17,8 @@ class FullpageWrapper extends React.Component {
 	}
 	afterLoad(origin, destination, direction) {
 		console.log('After load: ' + destination.index)
-	}
+	  }
+	
 	render() {
 		return (
 			<ReactFullpage
@@ -41,7 +45,8 @@ class FullpageWrapper extends React.Component {
 
 							<div className="section">
 								<h7>PASSIVE YIELD</h7>
-							</div>
+								<img src="../../assets/img/passive-yield.svg" />
+								<lottie-player src="../../assets/img/passive-yield.json"  speed="1"  style={{ width: '600px', height: '300px' }}  loop  autoplay></lottie-player>							</div>
 
 							<div className="section">
 								<h7>AUTOMATED STRATEGIES</h7>
