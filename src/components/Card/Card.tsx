66fd7531ff60dc328a847a239c0c21c519ecef9a
Ben Clabby <benclabby@gmail.com>
2021-08-15 16:11:02 -0400

    Restyle Modal Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import { lighten } from 'polished'
 
 const Card: React.FC = ({ children }) => <StyledCard>{children}</StyledCard>
 
 const StyledCard = styled.div`
-	background: ${(props) => props.theme.color.darkGrey[100]};
+	background: ${(props) => lighten(0.05, props.theme.color.darkGrey[200])};
 	border: 1px solid ${(props) => props.theme.color.darkGrey[200]};
 	border-radius: 12px;
-	box-shadow: inset 1px 1px 0px ${(props) => props.theme.color.darkGrey[100]};
 	display: flex;
 	flex: 1;
 	flex-direction: column;
