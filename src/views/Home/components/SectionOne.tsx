04b19dd6d7d8469a0358523e84344092fa439732
Vital <34991917+Vitalsine85@users.noreply.github.com>
2021-09-01 20:48:56 -0400

    comment out part of intro

--- src/views/Home/components/SectionOne.tsx
+++ src/views/Home/components/SectionOne.tsx
@@ -9,9 +9,9 @@ const SectionOne: React.FC = () => (
 		<HeroText>
 			With Polly Nests you can diversify your crypto portfoilio & earn passive
 			yield in just a few clicks! Nests utilize autonomous strategies that will
-			minimize the effort needed to maximize your returns. Polly DAO empowers
+			minimize the effort needed to maximize your returns.{/* Polly DAO empowers
 			users to curate their own Nests, as well as develop complex strategies of
-			their own.
+			their own. */}
 		</HeroText>
 	</>
 )
