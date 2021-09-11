b586535f8bc602f43e432a49b861db20ccccec57
Vital <34991917+Vitalsine85@users.noreply.github.com>
2021-09-11 17:37:51 -0400

    Fix fee widget

--- src/hooks/useFees.ts
+++ src/hooks/useFees.ts
@@ -1,17 +1,17 @@
 const getWithdrawalPenalty = (blocksSince: any) => {
   return blocksSince <= 0
     ? 0.99
-    : blocksSince <= 1200
+    : blocksSince <= 1800
     ? 0.5
-    : blocksSince <= 28800
+    : blocksSince <= 43200
     ? 0.25
-    : blocksSince <= 86400
+    : blocksSince <= 126000
     ? 0.12
-    : blocksSince <= 144000
+    : blocksSince <= 216000
     ? 0.08
-    : blocksSince <= 403200
+    : blocksSince <= 604800
     ? 0.04
-    : blocksSince <= 806400
+    : blocksSince <= 1296000
     ? 0.02
     : 0.001
 }
