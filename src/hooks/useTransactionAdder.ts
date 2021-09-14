b383cf959d97997d3695d4157b58862d32473140
Vital <34991917+Vitalsine85@users.noreply.github.com>
2021-09-14 15:02:24 -0400

    removed unused hooks

--- src/hooks/useTransactionAdder.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from 'contexts/Transactions'
-import { useContext } from 'react'
-
-const useTransactionAdder = () => {
-  const { onAddTransaction } = useContext(Context)
-  return { onAddTransaction }
-}
-
-export default useTransactionAdder
