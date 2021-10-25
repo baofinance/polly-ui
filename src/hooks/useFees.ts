18dd044a11eb739e58b9702e65ae5c9d2c3bfc56
Vital <34991917+Vitalsine85@users.noreply.github.com>
2021-10-25 16:19:29 -0400

    revert

--- src/hooks/useFees.ts
+++ src/hooks/useFees.ts
@@ -1,19 +1,19 @@
 const getWithdrawalPenalty = (blocksSince: any) => {
   return blocksSince <= 0
     ? 0.99
-    : blocksSince <= 1525
+    : blocksSince <= 1800
     ? 0.5
-    : blocksSince <= 36600
+    : blocksSince <= 43200
     ? 0.25
-    : blocksSince <= 109800
+    : blocksSince <= 126000
     ? 0.12
-    : blocksSince <= 183000
+    : blocksSince <= 216000
     ? 0.08
-    : blocksSince <= 512400
+    : blocksSince <= 604800
     ? 0.04
-    : blocksSince <= 1024800
+    : blocksSince <= 1296000
     ? 0.02
-    : 0.01
+    : 0.001
 }
 
 const useFees = (blockDiff: number) =>
