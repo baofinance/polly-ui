98221003e1624c9dcb50b41f0f813ab2765956c1
Vital <34991917+Vitalsine85@users.noreply.github.com>
2022-03-22 11:58:11 -0400

    Update useBlock.ts

--- src/hooks/base/useBlock.ts
+++ src/hooks/base/useBlock.ts
@@ -1,45 +1,36 @@
-import { useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
+import { useEffect, useState } from 'react'
 import Web3 from 'web3'
-import { provider } from 'web3-core'
-import React from 'react'
-// import debounce from 'debounce'
 
 const useBlock = () => {
-  const { chainId, library } = useWeb3React()
+  const [block, setBlock] = useState(0)
+  const { library } = useWeb3React()
 
-  const [blockNumber, setBlockNumber] = React.useState<number>()
-  React.useEffect((): any => {
-    if (!!library) {
-      let stale = false
+  useEffect(() => {
+    // const setBlockDebounced = debounce(setBlock, 300)
+    if (!library) return
+    const web3 = new Web3(library)
 
-      library
-        .getBlockNumber()
-        .then((blockNumber: number) => {
-          if (!stale) {
-            setBlockNumber(blockNumber)
-          }
-        })
-        .catch(() => {
-          if (!stale) {
-            setBlockNumber(null)
-          }
-        })
+    // const subscription = new Web3(ethereum).eth.subscribe(
+    //   'newBlockHeaders',
+    //   (error, result) => {
+    //     if (!error) {
+    //       setBlockDebounced(result.number)
+    //     }
+    //   },
+    // )
 
-      const updateBlockNumber = (blockNumber: number) => {
-        setBlockNumber(blockNumber)
+    const interval = setInterval(async () => {
+      const latestBlockNumber = await web3.eth.getBlockNumber()
+      if (block !== latestBlockNumber) {
+        setBlock(latestBlockNumber)
       }
-      library.on('block', updateBlockNumber)
+    }, 1000)
 
-      return () => {
-        stale = true
-        library.removeListener('block', updateBlockNumber)
-        setBlockNumber(undefined)
-      }
-    }
-  }, [library, chainId]) // ensures refresh if referential identity of library doesn't change across chainIds
+    return () => clearInterval(interval)
+  }, [library])
 
-  return blockNumber
+  return block
 }
 
 export default useBlock
