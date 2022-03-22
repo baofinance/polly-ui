19a7acf3835fa11d9ae5ecca12c192506995cd86
Vital <34991917+Vitalsine85@users.noreply.github.com>
2022-03-22 11:58:19 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import { Web3Provider } from '@ethersproject/providers'
-import { Web3ReactProvider } from '@web3-react/core'
+import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Web3 from 'web3'
 import { provider } from 'web3-core'
 import App from './App'
 
-function getLibrary(provider: provider, connector: any) {
+function getLibrary(provider: provider) {
 	return new Web3(provider)
 }
+
+const Web3ReactProviderReloaded = createWeb3ReactRoot('network')
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Web3ReactProvider getLibrary={getLibrary}>
-			<App />
+			<Web3ReactProviderReloaded getLibrary={getLibrary}>
+				<App />
+			</Web3ReactProviderReloaded>
 		</Web3ReactProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
