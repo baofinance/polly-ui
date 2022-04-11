5c7deb28e375c06bea1c754e86d3bb5fb75b8b65
Vital <34991917+Vitalsine85@users.noreply.github.com>
2022-04-10 23:35:35 -0400

    Update tokenBalance on new block

--- src/hooks/base/useTokenBalance.ts
+++ src/hooks/base/useTokenBalance.ts
@@ -4,11 +4,13 @@ import { useWeb3React } from '@web3-react/core'
 import { getBalance } from 'utils/erc20'
 import { provider } from 'web3-core'
 import useBao from './useBao'
+import useBlock from './useBlock'
 
 const useTokenBalance = (tokenAddress: string) => {
   const [balance, setBalance] = useState(new BigNumber(0))
   const { account } =  useWeb3React()
   const bao = useBao()
+  const block = useBlock()
 
   const fetchBalance = useCallback(async () => {
     const balance = await getBalance(bao, tokenAddress, account)
@@ -19,7 +21,7 @@ const useTokenBalance = (tokenAddress: string) => {
     if (account && bao && tokenAddress) {
       fetchBalance()
     }
-  }, [account, bao, setBalance, tokenAddress])
+  }, [account, bao, setBalance, tokenAddress, block])
 
   return balance
 }
