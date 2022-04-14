e64d929ff470c3ad887ce382e5051ed654c24982
Vital <34991917+Vitalsine85@users.noreply.github.com>
2022-04-14 17:59:29 -0400

    Update useAllowancev2.ts

--- src/hooks/base/useAllowancev2.ts
+++ src/hooks/base/useAllowancev2.ts
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
-import { provider } from 'web3-core'
 import useBao from './useBao'
 import { getAllowance } from 'utils/erc20'
 import { BigNumber } from 'bignumber.js'
