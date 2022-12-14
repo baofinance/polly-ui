42fd3e93745ec56bb1378c3fb23550766f68e9ba
Vital <rob@creative-element.net>
2022-12-14 15:11:55 -0500

    Styling and placement changes to Unlock

--- src/views/Farms/components/Unlock.tsx
+++ src/views/Farms/components/Unlock.tsx
@@ -24,10 +24,9 @@ const Unlock: React.FC = () => {
 
 	useEffect(() => {
 		if (!pollyContract || !account) return
-		getUnlockAmount(pollyContract, account)
-			.then((amount) => {
-				setPendingUnlock(new BigNumber(amount))
-			})
+		getUnlockAmount(pollyContract, account).then((amount) => {
+			setPendingUnlock(new BigNumber(amount))
+		})
 	}, [pollyContract, account, transactions])
 
 	const disabled = !account || !pollyContract || pendingUnlock.eq(0)
@@ -41,24 +40,48 @@ const Unlock: React.FC = () => {
 	) : (
 		<Fragment>
 			<Container>
-				<Row style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-					<Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+				<Row
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+				>
+					<Col
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+							textAlign: 'center',
+						}}
+					>
 						Unlockable Polly waiting for you to claim:
 						<Spacer size={'md'} />
 					</Col>
-					<Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
+					<Col
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+							textAlign: 'center',
+						}}
+					>
 						<StyledBadge style={{ fontSize: '1.5em', margin: '0 auto' }}>
-							<img src={baoIcon} alt='BAO' style={{ height: '1em' }} />
-							{' '}
+							<img src={baoIcon} alt="BAO" style={{ height: '1em' }} />{' '}
 							{pendingUnlock.eq(0) ? '-' : getDisplayBalance(pendingUnlock)}
 						</StyledBadge>
 						<Spacer size={'md'} />
 						<Button
-							width='fit-content'
+							width="fit-content"
 							disabled={disabled}
 							onClick={async (e: React.SyntheticEvent) => {
 								e.preventDefault()
-								const tx = pollyContract.methods.unlock().send({ from: account })
+								const tx = pollyContract.methods
+									.unlock()
+									.send({ from: account })
 								handleTx(tx, `Unlock ${getDisplayBalance(pendingUnlock)} Polly`)
 								tx.on('receipt', (receipt: any) => {
 									setPendingUnlock(new BigNumber(0))
